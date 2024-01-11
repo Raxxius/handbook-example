927b9c42ebff2a2ec130f62dce0b4daf95f5d7fa
Raxxius <raxxman@gmail.com>
2024-01-11 15:11:16 +0000

    add global state

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import './App.css'
+import { useState } from 'react'
 import QuoteComponent from './components/QuoteComponent'
 import CounterColourComponent from './components/CounterColourComponent'
 
 function App() {
+  // State
+  const colours = ["red", "blue", "green", "yellow", "orange"]
+  const [colour, setColour] = useState(colours[0])
+
   return (
     <>
       <div>Baw Medical Example React App</div>
