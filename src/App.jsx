b1977195f210babbdce305a8e43976ab7ed9c95f
Raxxius <raxxman@gmail.com>
2024-01-14 11:17:15 +0000

    updated state functions and imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import './App.css'
 import { useState } from 'react'
-import QuoteComponent from './components/QuoteComponent'
+import QuoteComponent from './components/QuoteComponent/QuoteComponent'
 import CounterColourComponent from './components/CounterColourComponent'
 
 function App() {
   // State
   const colours = ["red", "blue", "green", "yellow", "orange"]
-  const [colour, setColour] = useState(colours[0])
+  const [colourIndex, setColourIndex] = useState(0)
+  const colour = colours[colourIndex]
 
   // State functions
 
-  const changeColour = (prevColour) => {
-    console.log(changeColour)
+  const changeColour = (direction) => {
+    setColourIndex((prevIndex) => (prevIndex + direction + colours.length) % colours.length);
   }
 
   return (
@@ -21,7 +22,7 @@ function App() {
         colour={colour}
       />
       <CounterColourComponent 
-      
+        changeColour={changeColour}
       />
     </>
   )
